9226f3c8f333087d723278806d9b21dabc9a0a7f
atulchoudhary123276 <atulchoudhary123276@gmail.com>
2024-04-01 16:09:42 +0530

    "second commit with prod  profile"

--- FilterInterceptor/src/main/java/com/example/FilterInterceptor/config/TestConfig.java
+++ FilterInterceptor/src/main/java/com/example/FilterInterceptor/config/TestConfig.java
@@ -1,18 +1,35 @@
 package com.example.FilterInterceptor.config;
 
+import com.example.FilterInterceptor.filter.TestFilter;
 import com.example.FilterInterceptor.interceptor.TestInterceptor;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class TestConfig implements WebMvcConfigurer {
-    TestInterceptor testInterceptor;
-    TestConfig(TestInterceptor testInterceptor) {
-        this.testInterceptor = testInterceptor;
-    }
+//    TestInterceptor testInterceptor;
+//    TestConfig(TestInterceptor testInterceptor) {
+//        this.testInterceptor = testInterceptor;
+//    }
 
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(testInterceptor);
+        registry.addInterceptor(new TestInterceptor()).addPathPatterns("/*");
+
     }
+
+     /*  We can also execute filter classes based on specific URL.
+         For this we need to create one new configuration class
+         and need to register bean and will set which filter class we want to restrict.*/
+
+//    @Bean
+//    public FilterRegistrationBean<TestFilter> registrationBean(){
+//        FilterRegistrationBean<TestFilter> registrationBean=new FilterRegistrationBean<>();
+//        registrationBean.setFilter(new TestFilter());
+//        registrationBean.addUrlPatterns("/hi");
+//
+//        return registrationBean;
+//    }
 }
--- FilterInterceptor/src/main/java/com/example/FilterInterceptor/filter/TestFilter.java
+++ FilterInterceptor/src/main/java/com/example/FilterInterceptor/filter/TestFilter.java
@@ -9,10 +9,13 @@ import java.io.IOException;
 @Component
 public class TestFilter implements Filter {
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException, ServletException, IOException {
+    public void doFilter(ServletRequest servletRequest,
+                         ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException, ServletException, IOException {
         HttpServletResponse response = (HttpServletResponse) servletResponse;
         response.addHeader("custom-header","filter-value");
+        System.out.println("before do filtre");
         filterChain.doFilter(servletRequest,response);
+        System.out.println("after do filtre");
 
     }
 }
