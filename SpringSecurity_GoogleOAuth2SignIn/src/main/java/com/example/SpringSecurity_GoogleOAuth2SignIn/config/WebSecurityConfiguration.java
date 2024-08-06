e1375f9c6342337418f31095764325c68ec3ee5a
atulchoudhary123276 <atulchoudhary123276@gmail.com>
2024-08-06 10:33:32 +0530

    add junit testing program

--- SpringSecurity_GoogleOAuth2SignIn/src/main/java/com/example/SpringSecurity_GoogleOAuth2SignIn/config/WebSecurityConfiguration.java
+++ SpringSecurity_GoogleOAuth2SignIn/src/main/java/com/example/SpringSecurity_GoogleOAuth2SignIn/config/WebSecurityConfiguration.java
@@ -1,7 +1,9 @@
 package com.example.SpringSecurity_GoogleOAuth2SignIn.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -11,13 +13,30 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .cors()
+                .and()
                 .csrf()
                 .disable()
-                .antMatcher("/**")
+//                .antMatcher("/**")
                 .authorizeRequests()
                 .antMatchers("/", "/index.html","/welcome")
                 .permitAll()
                 .anyRequest()
                 .authenticated();
+
+        //both are same ...
+//        http
+//                .csrf()
+//                .disable()
+//                .cors()
+//                .and()
+//                .authorizeRequests()
+//                .antMatchers("/", "/index.html","/welcome")
+//                .permitAll()
+//                .anyRequest()
+//                .authenticated();
+
     }
+
+
 }
--- SpringSecurity_GoogleOAuth2SignIn/src/main/java/com/example/SpringSecurity_GoogleOAuth2SignIn/controller/GoogleserviceApplication.java
+++ SpringSecurity_GoogleOAuth2SignIn/src/main/java/com/example/SpringSecurity_GoogleOAuth2SignIn/controller/GoogleserviceApplication.java
@@ -10,10 +10,15 @@ import java.security.Principal;
 public class GoogleserviceApplication {
     @RequestMapping(value = "/user")
     public Principal user(Principal principal) {
+        System.out.println(principal);
         return principal;
     }
     @GetMapping("/welcome")
     public String gotoWelcome(){
         return "Welcome to Home Page";
+    }
+    @GetMapping("/page")
+    public String paeg(){
+        return "Welcome to MY Page";
     }
 }
